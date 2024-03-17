3ce8e912516a2d0254777a3429854237fae278fc
KigwanaAugustine <augustinekigz45@gmail.com>
2024-03-17 12:54:02 +0300

    Create end points for fetching temp and humidity data

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::group(['namespace' => 'App\Http\Controllers\Api\V1', 'prefix' => 'v1'], function () {
+    Route::get('hives/{hive_id}/temperature/{from_date}/{to_date}', 'HiveParameterDataController@getTemperatureForDateRange');
+    Route::apiResource('hives', 'HiveController');
+    Route::apiResource('hivetemperatures', 'HiveTemperatureController');
+});
+
+
+
+
