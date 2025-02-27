9ee5bfeb10fbfbf2962b1fe1149f20f91a1d5ce4
joshuamumbere <joshuamumbere71@gmail>
2025-02-27 15:55:34 +0300

    Images link implemented

--- resources/views/admin/team/index.blade.php
+++ resources/views/admin/team/index.blade.php
@@ -27,7 +27,7 @@
            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
               
                <th scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
-                   <img class="w-10 h-10 rounded-full" src="{{ $item->image_path }}" alt="Jese image"/>
+                   <img class="w-10 h-10 rounded-full" src="/images/{{ $item->image_path }}" alt="Jese image"/>
                    <div class="pl-3">
                        <div class="text-base font-semibold">{{ $item->name }}</div>
                        <div class="font-normal text-gray-500"></div>
