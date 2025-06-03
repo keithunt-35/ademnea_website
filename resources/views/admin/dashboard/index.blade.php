4d7f82486106d5f814c8101424ccad3ae0de0817
OkemaPaulMark <paulokema342@gmail.com>
2025-06-03 13:55:27 +0300

    improved dashboard

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -4,95 +4,98 @@
 <!-- the four cards above on the dashboard -->
 <div class="relative p-3 mt-2 overflow-x-auto shadow-md sm:rounded-lg">
     <div class="container-fluid py-4">
-    <div class="row min-vh-80 h-100">
-        <div class="col-12">
-        <div class="row">
-            <div class="col-xl-3 col-sm-6 grid-margin stretch-card">
-            <div class="card">
-                <div class="card-body">
+        <div class="row min-vh-80 h-100">
+            <div class="col-12">
                 <div class="row">
-                    <div class="col-9">
-                    <div class="d-flex align-items-center align-self-start">
-                        <h3 class="mb-0" style="color: #28a745;">{{$totalFarmers}}</h3>                 
+
+                    <!-- Total Farmers -->
+                    <div class="col-xl-3 col-sm-6 grid-margin stretch-card">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-9">
+                                        <div class="d-flex align-items-center align-self-start">
+                                            <h3 class="mb-0 text-success">{{$totalFarmers}}</h3>
+                                        </div>
+                                    </div>
+                                    <div class="col-3 text-end">
+                                        <i class="mdi mdi-account-group mdi-36px text-success"></i>
+                                    </div>
+                                </div>
+                                <h5 class="text-muted font-weight-normal">Total Farmers</h5>
+                                <p>This is the total number of farmers currently enrolled in the platform, 
+                                   each managing one or more farms through the system.</p>
+                            </div>
+                        </div>
                     </div>
+
+                    <!-- Total Farms -->
+                    <div class="col-xl-3 col-sm-6 grid-margin stretch-card">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-9">
+                                        <div class="d-flex align-items-center align-self-start">
+                                            <h3 class="mb-0 text-primary">{{$totalFarms}}</h3>
+                                        </div>
+                                    </div>
+                                    <div class="col-3 text-end">
+                                        <i class="mdi mdi-home-group mdi-36px text-primary"></i>
+                                    </div>
+                                </div>
+                                <h5 class="text-muted font-weight-normal">Total Farms</h5>
+                                <p>Total number of farms registered and monitored in the system, 
+                                   providing hive and team management per farmer.</p>
+                            </div>
+                        </div>
                     </div>
-                    <div class="col-3">
-                    <div class="icon icon-box-success ">
-                        <span class="mdi mdi-arrow-top-right icon-item"></span>
+
+                    <!-- Total Hives -->
+                    <div class="col-xl-3 col-sm-6 grid-margin stretch-card">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-9">
+                                        <div class="d-flex align-items-center align-self-start">
+                                            <h3 class="mb-0 text-warning">{{$totalHives}}</h3>
+                                        </div>
+                                    </div>
+                                    <div class="col-3 text-end">
+                                        <i class="mdi mdi-cube-outline mdi-36px text-warning"></i>
+                                    </div>
+                                </div>
+                                <h5 class="text-muted font-weight-normal">Total Hives</h5>
+                                <p>This represents all beehives managed across farms, 
+                                   each tracked individually for productivity and health.</p>
+                            </div>
+                        </div>
                     </div>
+
+                    <!-- Total Team -->
+                    <div class="col-xl-3 col-sm-6 grid-margin stretch-card">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-9">
+                                        <div class="d-flex align-items-center align-self-start">
+                                            <h3 class="mb-0 text-info">{{$totalTeams}}</h3>
+                                        </div>
+                                    </div>
+                                    <div class="col-3 text-end">
+                                        <i class="mdi mdi-account-tie mdi-36px text-info"></i>
+                                    </div>
+                                </div>
+                                <h5 class="text-muted font-weight-normal">Total Team</h5>
+                                <p>Shows the total number of teams or workers actively 
+                                   supporting farm operations and hive inspections.</p>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <h5 class="text-muted font-weight-normal"  style="color: #28a745;">Total Farmers</h5>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </p>
+
                 </div>
             </div>
-            </div>
-            <div class="col-xl-3 col-sm-6 grid-margin stretch-card">
-            <div class="card">
-                <div class="card-body">
-                <div class="row">
-                    <div class="col-9">
-                    <div class="d-flex align-items-center align-self-start">
-                        <h3 class="mb-0" style="color: #28a745;">{{$totalFarms}}</h3>
-                    </div>
-                    </div>
-                    <div class="col-3">
-                    <div class="icon icon-box-success">
-                        <span class="mdi mdi-arrow-top-right icon-item"></span>
-                    </div>
-                    </div>
-                </div>
-                <h5 class="text-muted font-weight-normal">Total Farms</h5>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </p>
-                </div>
-            </div>
-            </div>
-            <div class="col-xl-3 col-sm-6 grid-margin stretch-card">
-            <div class="card">
-                <div class="card-body">
-                <div class="row">
-                    <div class="col-9">
-                    <div class="d-flex align-items-center align-self-start">
-                        <h3 class="mb-0" style="color: #28a745;">{{$totalHives}}</h3>             
-                    </div>
-                    </div>
-                    <div class="col-3">
-                    <div class="icon icon-box-danger">
-                        <span class="mdi mdi-arrow-bottom-left icon-item"></span>
-                    </div>
-                    </div>
-                </div>
-                <h5 class="text-muted font-weight-normal">Total Hives</h5>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </p>
-                </div>
-            </div>
-            </div>
-            <div class="col-xl-3 col-sm-6 grid-margin stretch-card">
-            <div class="card">
-                <div class="card-body">
-                <div class="row">
-                    <div class="col-9">
-                    <div class="d-flex align-items-center align-self-start">
-                        <h3 class="mb-0" style="color: #28a745;">{{$totalTeams}}</h3>
-                        
-                    </div>
-                    </div>
-                    <div class="col-3">
-                    <div class="icon icon-box-success ">
-                        <span class="mdi mdi-arrow-top-right icon-item"></span>
-                    </div>
-                    </div>
-                </div>
-                <h5 class="text-muted font-weight-normal">Total Team</h5>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-                </div>
-            </div>
-            </div>
-        </div>
         </div>
     </div>
 </div>
 
-<!-- Add other things for the dasnboard here -->
-       
 @endsection
